a69e54aa587d1be37ffa66b53b8a270d7f20e3ff
Mustafa AL-Zubaidi <mdeveloper760@gmail.com>
2026-09-02 11:12:44 +0200

    Verify NuGet cache hit

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
 ﻿Console.WriteLine("Hello, DevOps!");
 Console.WriteLine("Version 1.0");
 Console.WriteLine("Environment: Development");
-Console.WriteLine("CI cache test");
+Console.WriteLine("CI cache test 2");
